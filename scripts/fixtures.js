6ffd162bf8ad470da4b7fed9983e222edc1c78ea
Scott Kipp <scott.kipp@gmail.com>
2016-12-29 20:17:42 -0500

    Checkpoint 2-19 complete

---
+++ scripts/fixtures.js
@@ -0,0 +1,47 @@
+// Example Album
+var albumPicasso = {
+    title: 'The Colors',
+    artist: 'Pablo Picasso',
+    label: 'Cubism',
+    year: '1881',
+    albumArtUrl: 'assets/images/album_covers/01.png',
+    songs: [
+        { title: 'Blue', duration: '4:26' },
+        { title: 'Green', duration: '3:14' },
+        { title: 'Red', duration: '5:01' },
+        { title: 'Pink', duration: '3:21' },
+        { title: 'Magenta', duration: '2:15' },
+    ]
+};
+
+//Another Example
+var albumMarconi = {
+    title: 'The Telephone',
+    artist: 'Guglielmo Marconi',
+    label: 'EM',
+    year: '1909',
+    albumArtUrl: 'assets/images/album_covers/20.png',
+    songs: [
+        { title: 'Hello Operator?', duration: '1:01' },
+        { title: 'Ring, ring, ring', duration: '5:01' },
+        { title: 'Fits in your pocket', duration: '3:21' },
+        { title: 'Can you hear me now?', duration: '3:14' },
+        { title: 'Wrong phone number', duration: '2:15'},
+    ]
+};
+
+//My ALbum
+var albumVeracity = {
+    title: 'Veracious',
+    artist: 'Baz Suufla',
+    label: 'CAD',
+    year: '1999',
+    albumArtUrl: 'assets/images/album_covers/19.png',
+    songs: [
+        { title: 'Electrons', duration: '3:01' },
+        { title: 'Splicing Your Genes', duration: '2:10' },
+        { title: 'We Are Atomic', duration: '3:33' },
+        { title: 'Rapture of Light', duration: '4:19' },
+        { title: 'Molecular Catastrophe', duration: '5:54'},
+    ]
+};
